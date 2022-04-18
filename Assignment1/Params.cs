d7517b3328891d887c631bcb24196136551eaaf9
Kalpeshpatil143 <102408063+Kalpeshpatil143@users.noreply.github.com>
2022-04-18 11:34:20 +0530

    Add files via upload

---
+++ Assignment1/Params.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    using System;
+    
+
+    namespace Param
+    {
+        class Params
+        {
+            public static void Main(string[] args)
+            {
+
+                int[] n1 = new int[100];
+                int sum = 0;
+                for (int i = 1; i < 6; ++i)
+                {
+                    Console.WriteLine("Enter the value {0}:", i);
+                    n1[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                sum = SumOfInteger(n1);
+
+
+                Console.WriteLine("Result={0}", sum);
+                // Console.ReadKey();
+            }
+            static int SumOfInteger(params int[] val)
+            {
+                int sum = 0;
+                foreach (int i in val)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+
+
+    }
+
+}
---
+++ Assignment1/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    using System;
+    
+    class Student
+    {
+        static void getStudentsList(string[] file)
+        {
+            int avgScore;
+            int maxAvgScore = Int32.MinValue;
+            List<string> names = new List<string>();
+            for (int i = 0; i < file.Length; i += 4)
+            {
+                avgScore = (Int32.Parse(file[i + 1]) +
+                            Int32.Parse(file[i + 2]) +
+                        Int32.Parse(file[i + 3])) / 3;
+
+                if (avgScore > maxAvgScore)
+                {
+                    maxAvgScore = avgScore;
+                    names.Clear();
+                    names.Add(file[i]);
+                }
+
+                else if (avgScore == maxAvgScore)
+                    names.Add(file[i]);
+            }
+
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.Write(names[i] + " ");
+            }
+
+            Console.WriteLine(maxAvgScore);
+        }
+
+        public static void Main()
+        {
+            string[] file = { "Kalpesh", "80", "50", "10", "Neha", "82", "50", "96", "jitesh", "79", "55", "69", "mahesh", "77", "80", "65" };
+            getStudentsList(file);
+        }
+    }
+
+}
